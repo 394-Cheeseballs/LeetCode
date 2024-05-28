9fa7fe8b8d4c0c22a4304c56cb831c8d38e40685
Laura Tauraite <141178363+394-Cheeseballs@users.noreply.github.com>
2024-05-28 19:26:10 +0300

    :ballot_box_with_check: palindrome-number [Runtime: 159 ms; Memory: 57.5 MB]

--- math/palindrome-number.js
+++ math/palindrome-number.js
@@ -6,8 +6,6 @@ var isPalindrome = function(x) {
     let stringX = x.toString();
     let splitX = stringX.split("");
     let reversedSplitX = splitX.reverse();
-    console.log(reversedSplitX)
     let reversedX = reversedSplitX.join("");
-    console.log(reversedX)
     return stringX == reversedX ?  true :  false;
 };
